4b7f2594de5c9bc7ff93a92d0d68ee179384441b
Vanderlei Chagas <vanderleichagas42@gmail.com>
2023-06-10 17:07:21 -0300

    feat: h2 database

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,10 +1,14 @@
 package com.example.demo.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "user_table")
 public class User implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
