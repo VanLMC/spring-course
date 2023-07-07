15afadb25790f0a04a0174986c0519311c407474
Vanderlei Chagas <vanderleichagas42@gmail.com>
2023-07-06 23:03:31 -0300

    add oneToMany relationship from order to orderItem

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "order_table")
@@ -24,6 +26,11 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
 
@@ -80,5 +87,9 @@ public class Order implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id, moment, client);
+    }
+
+    public Set<OrderItem> getItems(){
+        return items;
     }
 }
